39e9bd780ffad44a0bd059faa7a13afa0d997b8d
Fauzan Al Ghifary <m.fauzanghifary@gmail.com>
2022-03-20 10:36:55 +0700

    add note about solving problem strategy

---
+++ Section 04-06 Problem Solving Patterns/script.js
@@ -0,0 +1,43 @@
+/*  How to solve a problem
+
+1. Understand the problem
+
+can I restate the problem in my own words?
+What are the input?
+What are the outputs?
+Can the outputs be determined from the inputs?
+How should I label the impotant pieces of data that are part of the problem?
+
+2. Come up with concrete examples
+
+Start with simple examples
+Progress to more complex examples
+Explore examples with empty inputs
+Explore examples with invalid inputs
+Explore another edge cases
+
+3. Break it down
+
+Explicitly write out the steps you need to take (worries later about the syntax)
+
+4. Solve or simplify
+
+Solve the problem. If you can't, solve the simpler problem (worries later about complexity or edge cases).
+
+5. Look back and refactor
+
+It is a missed opportunity if you don't take the time to look at your code, look back, and reflect on it.
+
+can you check the result?
+can you derive the result differently?
+can you understand it at a glance?
+can you use the result or method for some other problem?
+can you improve the performance of your solution?
+Can you think of other ways to refactor?
+How have other people solved this problem?
+
+
+
+
+
+*/
